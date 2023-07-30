9bc3c4a7d7ecd337bbb6910c3ffcc202d8759058
Lee SeoWon <rymil9303@gmail.com>
2023-07-31 02:44:26 +0900

    feat: 주문 로직 생성(아이템아이디 매칭 수정 필요)

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const ItemRouter = require("./routes/item");
+const OrderRouter = require("./routes/order_item");
 const app = express();
 const port = 3005;
 
 app.use(express.json());
-app.use("/api", [ItemRouter]);
+app.use("/api", [ItemRouter, OrderRouter]);
 
 app.listen(port, () => {
   console.log(port, "서버 실행");
---
+++ routes/order_item.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const { Item, Order_Item } = require("../models");
+const router = express.Router();
+
+//주문 생성
+router.post("/order/:itemId", async (req, res) => {
+  const { itemId } = req.params;
+  const { amount, state } = req.body;
+
+  const Items = await Item.findOne({ where: { itemId } });
+  if (Items) {
+    return res.status(400).json({ message: "상품을 찾을 수 없습니다." });
+  }
+
+  const order = await Order_Item.create({
+    ItemId: itemId,
+    amount,
+    state,
+  });
+  return res.status(200).json({ data: order });
+});
+
+//주문조회
+router.get("/order", async (req, res) => {
+  // Order_Item 테이블에서 참조하는 모든 필요한 정보를 찾는다.
+  const getOrder = await Order_Item.findAll({
+    attributes: [
+      "orderItemId",
+      "ItemId",
+      "amount",
+      "state",
+      "createdAt",
+      "updatedAt",
+    ],
+    // 내림차순으로 정렬
+    order: [["createdAt", "DESC"]],
+  });
+  // 댓글이 잘 조회되었을 경우 Order_Item 정보를 보여준다.
+  return res.status(200).json({ data: getOrder });
+});
+module.exports = router;
