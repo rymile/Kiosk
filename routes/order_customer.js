471cc3115e73046526caffb93321d86a5caaead3
Lee SeoWon <rymil9303@gmail.com>
2023-08-03 20:35:55 +0900

    fix: 오더커스터머 로직 추가 및 보완

--- routes/order_customer.js
+++ routes/order_customer.js
@@ -4,7 +4,10 @@ const { Item, Order_Customer, Item_Order_Customer } = require("../models");
 class OrderCustomerRouter {
   constructor() {
     this.router = express.Router();
+    //상품 주문 ID를 발급
     this.router.post("/OrderCustomer", this.orderC.bind(this));
+    // 상품 주문
+    this.router.post("/putOrder/:ordercustomerId", this.PutC.bind(this));
   }
 
   getOrderId() {
@@ -42,6 +45,45 @@ class OrderCustomerRouter {
       return res
         .status(201)
         .json({ orderID, totalPrice, message: "주문 완료" });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "서버 에러가 발생했습니다." });
+    }
+  }
+
+  async PutC(req, res) {
+    const { ordercustomerId } = req.params;
+    try {
+      const order = await Order_Customer.findOne({
+        where: { ordercustomerId },
+        include: { model: Item },
+      });
+
+      if (!order) {
+        res.status(400).json({ message: "주문을 찾을 수 없습니다." });
+      }
+      if (order.state === true) {
+        return res.state(401).json({ message: "주문이 이미 존재합니다." });
+      }
+      order.state = true;
+
+      return res.status(200).json({
+        orderID: order.orderID,
+        totalPrice: order.totalPrice,
+        message: "주문이 수정되었습니다.",
+      });
+
+      // for (const item of order.Items) {
+      //   const itemOrder = await Item_Order_Customer.findOne({
+      //     where: { orderId: order.id, itemId: item.id },
+      //   });
+
+      //   if (itemOrder) {
+      //     const quantity = itemOrder.amount;
+      //     item.amount -= quantity;
+      //     await item.save();
+      //   }
+      // }
     } catch (error) {
       console.error(error);
       return res.status(500).json({ message: "서버 에러가 발생했습니다." });
